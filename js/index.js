20593a1b46e67af55cf2c322b3d368f466fec189
snail <727605760@qq.com>
2017-04-27 14:42:58 +0800

    更新

--- js/index.js
+++ js/index.js
@@ -24,21 +24,23 @@ $(function () {
 	$(".hotcity-list li").find(".city").addClass("hide");
 	$(".hotcity-list li").click(function (){
 		if ($(this).find(".city").hasClass("hide")) {
-			$(this).css("border-bottom", "1px dashed #eee");
+			$(this).find(".province").css("border-bottom", "1px dashed #eee");
 			$(this).find(".city").toggle(300).removeClass("hide");
 		}else {
 			$(this).find(".city").toggle(300).addClass("hide");
-			$(this).css("border-bottom", "1px solid #eee");
+			$(this).find(".province").css("border-bottom", "1px solid #eee");
 		}
 		
 	});
-	
 	
 	// 删除导航下划线
 	$(".navbar-list ul li a").click(function () {
 		$(".navbar-list ul li em").addClass("hide");
 		$(this).siblings().removeClass("hide");
 	});
+	
+	
+	// 轮播图
 	var iNum = 0;
 	var oTimer = setInterval(function () {
 		iNum ++;
@@ -49,7 +51,7 @@ $(function () {
 			iNum = 0;
 			$(".home-banner-list").css("left", 0);
 		}
-	}, 5000);
+	}, 2000);
 });
 
 
